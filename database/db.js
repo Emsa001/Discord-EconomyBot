0fa7a09bb037c24eda44538dd1cdfb3052396f27
Emsa001 <emikscur123@gmail.com>
2021-11-25 15:21:21 +0100

    main

--- database/db.js
+++ database/db.js
@@ -1,11 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize(
-  "5795_richfun",
-  "5795_richfun",
-  "Wi7#k1@8#K*yU",
-  {
-    host: "evelinka99.atthost24.pl",
-    dialect: "mysql",
-  }
-);
+module.exports = new Sequelize("db_name", "db_username", "db_password", {
+  host: "db_host",
+  dialect: "mysql",
+});
