5fec6ad4caf0b20ebb50969a143de10877bf8a2b
Emsa001 <emikscur123@gmail.com>
2021-11-25 15:16:53 +0100

    main

--- database/db.js
+++ database/db.js
@@ -1,11 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize(
-  "db_name",
-  "db_username",
-  "db_password",
-  {
-    host: "db_host",
-    dialect: "mysql",
-  }
-);
+module.exports = new Sequelize("db_name", "db_username", "db_passwordasd", {
+  host: "db_host",
+  dialect: "mysql",
+});
